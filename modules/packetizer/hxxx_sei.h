fcartegnie/vlc-sdi

hxxx_sei: add support for afd payload

modules/packetizer/hxxx_sei.h
@@ -31,6 +31,7 @@ enum hxxx_sei_type_e
 enum hxxx_sei_t35_type_e
 {
     HXXX_ITU_T35_TYPE_CC,
+    HXXX_ITU_T35_TYPE_AFD,
 };
 
 typedef struct
@@ -49,6 +50,7 @@ typedef struct
                     const uint8_t *p_data;
                     size_t i_data;
                 } cc;
+                uint8_t afd;
             } u;
         } itu_t35;
         struct

LTNGlobal-opensource/vlc-videolabs

modules/packetizer/hxxx_sei.h
@@ -34,6 +34,7 @@ enum hxxx_sei_type_e
 enum hxxx_sei_t35_type_e
 {
     HXXX_ITU_T35_TYPE_CC,
+    HXXX_ITU_T35_TYPE_AFD,
 };
 
 typedef struct
@@ -52,6 +53,7 @@ typedef struct
                     const uint8_t *p_data;
                     size_t i_data;
                 } cc;
+                int8_t afd;
             } u;
         } itu_t35;
         struct